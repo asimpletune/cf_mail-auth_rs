7c257499268b2e23c69db95b9e98218c062adf97
Spencer Scorcelletti <contact@spenc.es>
2023-06-24 17:48:17 +0200

    Basic hello world sanity check

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,22 @@
 use worker::*;
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or("unknown region".into())
+    );
+}
 
 #[event(fetch)]
-async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
+async fn main(req: Request, _: Env, _: Context) -> Result<Response> {
+    log_request(&req);
+
+    // Optionally, get more helpful error messages written to the console in the case of a panic.
+    utils::set_panic_hook();
+
     Response::ok("Hello, World!")
 }
